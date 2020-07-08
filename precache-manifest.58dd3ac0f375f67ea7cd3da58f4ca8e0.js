086f1904059e650186a1f5007aca855f25a743b3
Stu Bolderson <stu.bolderson@aol.com>
2020-07-08 22:03:20 +0200

    Updates

---
+++ precache-manifest.58dd3ac0f375f67ea7cd3da58f4ca8e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9357b597f6da868168d7204b671ca3",
+    "url": "/reactfriends/index.html"
+  },
+  {
+    "revision": "4ca8243a87127decba97",
+    "url": "/reactfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e8e7a976592df051b309",
+    "url": "/reactfriends/static/css/main.2e60c918.chunk.css"
+  },
+  {
+    "revision": "4ca8243a87127decba97",
+    "url": "/reactfriends/static/js/2.efca6260.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactfriends/static/js/2.efca6260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8e7a976592df051b309",
+    "url": "/reactfriends/static/js/main.63f89af1.chunk.js"
+  },
+  {
+    "revision": "511e20ba01bf942da234",
+    "url": "/reactfriends/static/js/runtime-main.ae6aec8b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/reactfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
